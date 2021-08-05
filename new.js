ad03c8c30d9c90d0aa702c3663c6e60565f40b9b
Stepan <digital-souris@ya.ru>
2021-08-05 17:33:38 +0300

    add event page

--- new.js
+++ new.js
@@ -1,4 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function(){
+
+    $(document).on('click', '.load_more', function(){
+
+        var targetContainer = $('.product-item__wrap'),          //  Контейнер, в котором хранятся элементы
+            url =  $('.load_more').attr('data-url');    //  URL, из которого будем брать элементы
+
+        if (url !== undefined) {
+            $.ajax({
+                type: 'GET',
+                url: url,
+                dataType: 'html',
+                success: function(data){
+
+                    //  Удаляем старую навигацию
+                    $('.catalog__info-view').remove();
+
+                    var elements = $(data).find('.product-item'),  //  Ищем элементы
+                        pagination = $(data).find('.catalog__info-view');//  Ищем навигацию
+
+                    targetContainer.append(elements);   //  Добавляем посты в конец контейнера
+                    targetContainer.append(pagination); //  добавляем навигацию следом
+
+                }
+            })
+        }
+
+    });
+
     $('.footer__top').click(function () {
         var body = $('html, body');
         body.animate({scrollTop: 0}, 500)
@@ -7,6 +35,31 @@ $(document).ready(function () {
         var parent = $(this).parent()
         parent.find('.footer__list').slideToggle()
     })
+    $('[data-basket]').on('click', function(){
+        // var count = $(this).data('count');
+        var count = $(this).parents('[data-parent]').find('[data-count]').val();
+        var product_id = $(this).data('basket');
+        var action = 'add';
+        console.log(product_id);
+        $.ajax({
+            dataType: 'json',
+            data: {'count':count, 'action':action, 'product_id':product_id},
+            type: 'POST',
+            url: '/ajax/card.php',
+            success: function(response){
+                // if (response.status)
+                // {
+                //  console.log('----------------->',response.total_price);
+                //  response.total_price =  response.total_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+                //  $(document).find('[data-totalBasket]').html(response.total_price+'₽');
+                //  // $('[data-totalBasket]').html(response.total_price+'₽');
+                //  $('[data-totalMob]').html(response.total_price+'₽');
+                //  $('[data-allCount]').html(response.total_quantity);
+                // }
+            }
+        });
+    });
+
     $('.topline__filter').click(function () {
         $('.filter[data-theme=main]').toggleClass('is-open')
     })
@@ -23,8 +76,12 @@ $(document).ready(function () {
         $(this).closest('.mobile-filter').removeClass('is-open')
     })
     $('.news__footer a').click(function () {
-        if ($(this).find('.share2').length) {
+        console.log($(this).find('.ya-share2').length)
+        if ($(this).find('.ya-share2').length) {
             $('.ya-share2__popup').toggleClass('ya-share2__popup_visible')
+            return false
         }
     })
-})
+});
+
+
